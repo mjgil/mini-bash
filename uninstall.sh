8bbf2af033352be90f15773d36b9e191cf0b1621
Malcom Gilbert <malcomgilbert@gmail.com>
2019-10-18 05:46:12 -0500

    add uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 
+# needs to be ran from root mini-bash directory
+for i in scripts/*.sh; do
+  # echo "$(pwd)/$i"
+  scriptname=${i##scripts/}
+  basename=${scriptname%%.sh}
+  echo "removing link $(pwd)/$i -> /usr/bin/$basename"
+  sudo rm /usr/bin/$basename
+done
