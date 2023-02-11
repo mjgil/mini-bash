896bf057859ecac98886dd0598cd7a971e0ed992
Malcom Gilbert <malcomgilbert@gmail.com>
2023-02-11 13:42:51 -0600

    switch to /usr/local/bin

--- install-local.sh
+++ install-local.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+dir_name=/usr/local/bin
+
 # needs to be ran from root mini-bash directory
 for i in scripts/*.sh; do
   # echo "$(pwd)/$i"
   scriptname=${i##scripts/}
   basename=${scriptname%%.sh}
-  echo "linking $(pwd)/$i to /usr/bin/$basename"
-  sudo ln -s "$(pwd)/$i" /usr/bin/$basename
+  echo "linking $(pwd)/$i to $dir_name/$basename"
+  sudo ln -s "$(pwd)/$i" $dir_name/$basename
 done
--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+dir_name=/usr/local/bin
+
 # needs to be ran from root mini-bash directory
 for i in scripts/*.sh; do
   # echo "$(pwd)/$i"
   scriptname=${i##scripts/}
   basename=${scriptname%%.sh}
-  echo "removing link $(pwd)/$i -> /usr/bin/$basename"
-  sudo rm /usr/bin/$basename
+  echo "removing link $(pwd)/$i -> $dir_name/$basename"
+  sudo rm $dir_name/$basename
 done
